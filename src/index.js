35fe65254f0d7312635f7263b3fd9155fb7e345e
Eshragh Ghoodjani <eshraghg@gmail.com>
2019-05-07 08:11:55 +0430

    App basics

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+state = {
+            lat: null,
+            errorMassage: ''
+        }
+
+    
+        
+    componentDidMount () {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMassage: err.message})
+        )}
+
+    render () {
+        if (this.state.errorMassage && !this.state.lat) {
+                return <div> Error: {this.state.errorMassage} </div>
+        }
+
+        if (!this.state.errorMassage && this.state.lat) {
+            return <div> Latitude: {this.state.lat} </div>
+        }
+
+        return <div>Loading...</div>
+        
+    }
+};
+
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
